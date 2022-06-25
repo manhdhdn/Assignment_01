2fd82b8a17715c64a4b46f5a563aec9365836f3d
manhdhdn <huumanh.bmt.dn@gmail.com>
2022-06-25 13:16:04 +0700

    Add Login, Get member by ID or (Email and Pass), Add new member functions.

--- BusinessObject/MemberObject.cs
+++ BusinessObject/MemberObject.cs
@@ -5,12 +5,17 @@ namespace BusinessObject
     public class MemberObject
     {
         [Key]
+        [Range(1, int.MaxValue)]
         public int MemberID { get; set; }
+        [Range(0, 50)]
         public string MemberName { get; set; } = null!;
         [EmailAddress]
         public string Email { get; set; } = null!;
+        [Range(6, 20)]
         public string Password { get; set; } = null!;
+        [Range(0, 50)]
         public string City { get; set; } = null!;
+        [Range(0, 50)]
         public string Country { get; set; } = null!;    
     }
 }
--- DataAccess/Repository/IMemberRepository.cs
+++ DataAccess/Repository/IMemberRepository.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BusinessObject;
 
 namespace DataAccess.Repository
 {
     public interface IMemberRepository
     {
+        bool Login(string username, string password);
+        IEnumerable<MemberObject> GetMembers();
+        MemberObject GetMember(int memberID, string? email, string? password);
+        void InsertMember(MemberObject member);
+        void UpdateMember(MemberObject member);
+        void DeleteMember(int memberID);
     }
 }
